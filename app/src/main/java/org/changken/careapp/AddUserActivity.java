6911764990fa250f252bf8ff5ff86c1b5b6bfa50
changken <pp24513@gmail.com>
2019-06-06 17:43:38 +0800

    註冊帳號頁面ui、邏輯修正
    1.ui修正
    2.加上檢查帳號是否已註冊的邏輯
    3.加上提示註冊完成的對話框!!!!

--- app/src/main/java/org/changken/careapp/AddUserActivity.java
+++ app/src/main/java/org/changken/careapp/AddUserActivity.java
@@ -3,6 +3,8 @@ package org.changken.careapp;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
@@ -15,6 +17,7 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.changken.careapp.datamodels.AirTableListResponse;
 import org.changken.careapp.datamodels.AirTableResponse;
 import org.changken.careapp.datamodels.User;
 import org.changken.careapp.models.ModelCallback;
@@ -22,6 +25,8 @@ import org.changken.careapp.models.UserModel;
 import org.changken.careapp.tools.Helper;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Response;
@@ -32,6 +37,8 @@ public class AddUserActivity extends AppCompatActivity {
             emailTextView, phoneTextView, addressTextView, birthdayEditText;
     private Button submitButton, datePickerButton;
     private UserModel userModel;
+    //設定警告視窗
+    private AlertDialog progressDialog;
 
     /**
      * 初始化相關View元件
@@ -48,6 +55,7 @@ public class AddUserActivity extends AppCompatActivity {
         submitButton = (Button) findViewById(R.id.submit_button);
         datePickerButton = (Button) findViewById(R.id.data_picker_button);
         userModel = new UserModel();
+        progressDialog = Helper.createProgressDialog(this, "註冊中...");
     }
 
     @Override
@@ -76,51 +84,114 @@ public class AddUserActivity extends AppCompatActivity {
             if (name.length() > 0 && idNumber.length() > 0 && pw.length() > 0 &&
                     email.length() > 0 && phone.length() > 0 && address.length() > 0 &&
                     birthday.length() > 0) {
-
-                //設定警告視窗
-                final AlertDialog progressDialog = Helper.createProgressDialog(this, "註冊中...");
-
-                //建立並執行新增資料的請求
-                userModel.add(new User(
-                        name,
-                        idNumber,
-                        pw,
-                        email,
-                        phone,
-                        address,
-                        birthday
-                ), new ModelCallback<AirTableResponse<User>>() {
-                    @Override
-                    public void onProgress() {
-                        //顯示它!
-                        progressDialog.show();
-                    }
-
-                    @Override
-                    public void onProcessEnd() {
-                        //關掉alert視窗
-                        progressDialog.dismiss();
-                    }
-
-                    @Override
-                    public void onResponseSuccess(Call<AirTableResponse<User>> call, Response<AirTableResponse<User>> response) {
-                        Toast.makeText(AddUserActivity.this, "新增成功!:)", Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onResponseFailure(Call<AirTableResponse<User>> call, Response<AirTableResponse<User>> response) {
-                        Toast.makeText(AddUserActivity.this, "新增失敗!伺服器回應有些怪怪的~~", Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onFailure(Call<AirTableResponse<User>> call, Throwable t) {
-                        Toast.makeText(AddUserActivity.this, "新增失敗!可能是網路沒有通唷~", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                //檢查帳號是否有重複
+                checkIfUserIsExisted(name, idNumber, pw, email, phone, address, birthday);
             } else {
-                Toast.makeText(AddUserActivity.this, "新增失敗!請填寫所有的欄位!", Toast.LENGTH_SHORT).show();
+                showToastMessage("新增失敗!請填寫所有的欄位!");
             }
         });
+    }
+
+    private void addUser(String name, String idNumber, String pw, String email, String phone, String address, String birthday) {
+        //建立並執行新增資料的請求
+        userModel.add(new User(
+                name,
+                idNumber,
+                pw,
+                email,
+                phone,
+                address,
+                birthday
+        ), new ModelCallback<AirTableResponse<User>>() {
+            @Override
+            public void onProgress() {
+                //顯示它!
+                progressDialog.show();
+            }
+
+            @Override
+            public void onProcessEnd() {
+                //關掉alert視窗
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void onResponseSuccess(Call<AirTableResponse<User>> call, Response<AirTableResponse<User>> response) {
+                showSuccessfulAddUserDialog();
+            }
+
+            @Override
+            public void onResponseFailure(Call<AirTableResponse<User>> call, Response<AirTableResponse<User>> response) {
+                showToastMessage("新增失敗!伺服器回應有些怪怪的~~");
+            }
+
+            @Override
+            public void onFailure(Call<AirTableResponse<User>> call, Throwable t) {
+                showToastMessage("新增失敗!可能是網路沒有通唷~");
+            }
+        });
+    }
+
+    private void checkIfUserIsExisted(String name, String idNumber, String pw, String email, String phone, String address, String birthday) {
+        Map<String, String> queryMap = new HashMap<>();
+        queryMap.put("view", "Grid%20view");
+        queryMap.put("filterByFormula", "{user_id} = '" + idNumber + "'");
+
+        userModel.list(queryMap, new ModelCallback<AirTableListResponse<User>>() {
+
+            @Override
+            public void onProgress() {
+                //顯示它!
+                progressDialog.show();
+            }
+
+            @Override
+            public void onProcessEnd() {
+                //關掉alert視窗
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void onResponseSuccess(Call<AirTableListResponse<User>> call, Response<AirTableListResponse<User>> response) {
+                //如果筆數大於0，就表示已有該名使用者
+                if (response.body().getRecords().size() > 0) {
+                    showToastMessage("已有該使用者了!");
+                } else {
+                    addUser(name, idNumber, pw, email, phone, address, birthday);
+                }
+            }
+
+            @Override
+            public void onResponseFailure(Call<AirTableListResponse<User>> call, Response<AirTableListResponse<User>> response) {
+                showToastMessage("查詢失敗!伺服器回應有些怪怪的~~");
+            }
+
+            @Override
+            public void onFailure(Call<AirTableListResponse<User>> call, Throwable t) {
+                showToastMessage("查詢失敗!可能是網路沒有通唷~");
+            }
+        });
+    }
+
+    private void showToastMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void showSuccessfulAddUserDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("註冊成功!")
+                .setMessage("註冊成功!")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        goToLoginPage();
+                    }
+                }).show();
+    }
+
+    private void goToLoginPage() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 
     /**
